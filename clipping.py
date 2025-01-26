5b332ae711b81459f00b36efda65d5bcffb9f428
ffaann02 <rudfaanmaimahad@gmail.com>
2025-01-26 21:15:44 +0700

    refactor: handle edge case for all possible boundary

--- clipping.py
+++ clipping.py
@@ -1,33 +1,56 @@
-min_window = input().split()
-max_window = input().split()
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-x_min, y_min, x_max, y_max = int(min_window[0]), int(min_window[1]), int(max_window[0]), int(max_window[1])
+min_window = input("X_min Y_min: ").split()
+max_window = input("X_max Y_max: ").split()
+
+x_min, y_min = int(min_window[0]), int(min_window[1])
+x_max, y_max = int(max_window[0]), int(max_window[1])
 
 def get_m(x1, y1, x2, y2):
     # y = mx + p
     m = (y2 - y1) / (x2 - x1)
-
     return m
 
-def get_p(m, x = None, y = None):
+def get_p(m, x, y):
     return y - m * x
 
-def get_boundary_x(x_min, m, p):
-    return int(x_min), int(m * x_min + p)
+def get_intersection_point(x, y, m, p):
+    intersection_point = Point(0, 0)
+    if x < x_min and y_min < y < y_max:
+        intersection_point.x = x_min
+        intersection_point.y = m * x_min + p
+    
+    elif x > x_max and y_min < y < y_max:
+        intersection_point.x = x_max
+        intersection_point.y = m * x_max + p
 
-def get_boundary_y(y_max, m, p):
-    return int((y_max - p) / m), int(y_max)
+    elif y < y_min and x_min < x < x_max:
+        intersection_point.x = (y_min - p) / m
+        intersection_point.y = y_min
+    
+    elif y > y_max and x_min < x < x_max:
+        intersection_point.x = (y_max - p) / m
+        intersection_point.y = y_max
 
-A = input().split()
-B = input().split()
-
-m = get_m(int(A[0]), int(A[1]), int(B[0]), int(B[1]))
-print(f"slope: {m:.2f}")
-
-x1, y1 = int(A[0]), int(A[1])
-x2, y2 = int(B[0]), int(B[1])
-
-p1 = get_p(m, x1, y1)
-p2 = get_p(m, x2, y2)
-
-print(get_boundary_x(x_min, m, p1), get_boundary_y(y_max, m, p1))
+    return intersection_point
+    
+while 1:
+    point1 = input("Point 1: ").split()
+    if point1[0] == "-":
+        break
+    point2 = input("Point 2: ").split()
+    if point2[0] == "-":
+        break
+    else:
+        x1, y1 = int(point1[0]), int(point1[1])
+        x2, y2 = int(point2[0]), int(point2[1])
+        m = get_m(x1, y1, x2, y2)
+        print(f"Slope: {m:.2f}")
+        p = get_p(m, x1, y1)
+        point1_intersection = get_intersection_point(x1, y1, m, p)
+        point2_intersection = get_intersection_point(x2, y2, m, p)
+        print(f"Intersection point 1: ({point1_intersection.x:.2f}, {point1_intersection.y:.2f})")
+        print(f"Intersection point 2: ({point2_intersection.x:.2f}, {point2_intersection.y:.2f})")
